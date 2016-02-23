76a16e9afdeb9c7c27e34e0d5a765f350f0e7f82
EnthyrosVanAkel <thewakosmanclub@gmail.com>
2016-02-23 08:09:08 -0600

    ccambios

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -55,7 +55,7 @@
                     <!-- menu prile quick info -->
                     <div class="profile">
                         <div class="profile_pic">
-                            <img src="/Admin/images/img.jpg" alt="..." class="img-circle profile_img">
+                            <img src="/Admin/images/user.png" alt="..." class="img-circle profile_img">
                         </div>
                         <div class="profile_info">
                             <span>Welcome,</span>
@@ -117,7 +117,7 @@
                         <ul class="nav navbar-nav navbar-right">
                             <li class="">
                                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                    <img src="/Admin/images/img.jpg" alt="">{{ $user->name }}
+                                    <img src="/Admin/images/user.png" alt="">{{ $user->name }}
                                     <span class=" fa fa-angle-down"></span>
                                 </a>
                                 <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
